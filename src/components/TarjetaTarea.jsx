adc0cda8ec944d3504d1587f5701b8b9d1b6cee5
fheralberto <fheralberto@gmail.com>
2023-08-27 05:53:38 -0600

    Sin id en cada tarjeta

--- src/components/TarjetaTarea.jsx
+++ src/components/TarjetaTarea.jsx
@@ -1,17 +1,16 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 
 const TarjetaTarea = ({ tarea }) => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <section 
-      className="bg-slate-800 hover:bg-slate-700 hover:cursor-pointer px-4 py-2 flex flex-col justify-between gap-2 rounded-md" 
-      onClick={()=>{
-        router.push(`/editar/${tarea.id}`)    
-    }}>
-      <h2 className="text-xl font-medium">
-        <span className="font-normal">{tarea.id}. </span> {tarea.titulo}
-      </h2>
+    <section
+      className="bg-slate-800 hover:bg-slate-700 hover:cursor-pointer px-4 py-2 flex flex-col justify-between gap-2 rounded-md"
+      onClick={() => {
+        router.push(`/editar/${tarea.id}`);
+      }}
+    >
+      <h2 className="text-xl font-medium">{tarea.titulo}</h2>
       <p className="text-lg font-thin">{tarea.descripcion}</p>
       <p className="text-sm font-light">
         {new Date(tarea.createdAt).toLocaleDateString()}
